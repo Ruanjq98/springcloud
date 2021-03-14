f0343cda619b1a4f9c536a71ac090e6afca05217
ruanjq <ruanjq@dcits.com>
2021-03-14 15:12:33 +0800

    1

---
+++ cloud-getway-geteay9527/src/main/java/com/rjq/cloud/config/GateWayConfig.java
@@ -0,0 +1,25 @@
+package com.rjq.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: ruanjq
+ * @DateTime: 2021/3/14$ 15:06$
+ */
+@Configuration
+public class GateWayConfig {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+
+}
